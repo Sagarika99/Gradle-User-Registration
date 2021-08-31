8f7bb9d6466bbc030b973bf861483654be5c5152
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-09-01 00:12:22 +0530

    Validate Email ID

--- lib/src/main/java/com/bridgelabz/userregistrationgradle/UserRegistration.java
+++ lib/src/main/java/com/bridgelabz/userregistrationgradle/UserRegistration.java
@@ -5,15 +5,21 @@ import java.util.regex.Matcher;
 
 public class UserRegistration {
 
-	public static final String Name_Pattern = "^[A-Z a-z]{3,}";
+	public static final String Name_Pattern = "^[A-Z][a-z]{3,}";
+	public static final String Email_ID = "^([a-z0-9]{3,}[\\.-]?[a-z0-9]{3,})@([a-z\\d]{3,})[\\.]?([a-z]{3})$";
 	
 	public boolean FirstName(String fname) {
-		Pattern pattern = Pattern.compile(fname);
+		Pattern pattern = Pattern.compile(Name_Pattern);
 		return pattern.matcher(fname).matches();
 	}
 	
 	public boolean LastName(String lname) {
-		Pattern pattern = Pattern.compile(lname);
+		Pattern pattern = Pattern.compile(Name_Pattern);
 		return pattern.matcher(lname).matches();
+	}
+	
+	public boolean EmailId(String email) {
+		Pattern pattern = Pattern.compile(Email_ID);
+		return pattern.matcher(email).matches();
 	}
 }
--- lib/src/test/java/com/bridgelabz/userregistrationgradle/UserRegistrationRunner.java
+++ lib/src/test/java/com/bridgelabz/userregistrationgradle/UserRegistrationRunner.java
@@ -4,18 +4,119 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class UserRegistrationRunner {
-
+	
+	//first name
 	@Test
-	public void givenFirstName_whenProper_shouldReturnTrue() {
+	public void givenFirstName_WhenProper_ShouldReturnTrue() {
 		UserRegistration registration = new UserRegistration();
 		boolean result = registration.FirstName("Sagarika");
 		Assert.assertEquals(true,result);
 	}
 	
 	@Test
+	public void givenFirstName_WhenShort_ShouldReturnFalse() {
+		UserRegistration registration = new UserRegistration();
+		boolean result = registration.FirstName("Sa");
+		Assert.assertEquals(false, result);
+	}
+	
+	@Test
+	public void givenFirstName_WhenWithSpChars_ShouldReturnFalse() {
+		UserRegistration registration = new UserRegistration();
+		boolean result = registration.FirstName("S@garika");
+		Assert.assertEquals(false, result);
+	}
+	
+	@Test
+	public void givenFirstName_WhenWithoutCaps_ShouldReturnFalse() {
+		UserRegistration registration = new UserRegistration();
+		boolean result = registration.FirstName("sagarika");
+		Assert.assertEquals(false, result);
+	}
+	
+	//last name
+	@Test
 	public void givenLastName_whenProper_shouldReturnTrue() {
 		UserRegistration registration = new UserRegistration();
 		boolean result = registration.LastName("Shinde");
 		Assert.assertEquals(true,result);
+	}
+	
+	@Test
+	public void givenLastName_WhenShort_ShouldReturnFalse() {
+		UserRegistration registration = new UserRegistration();
+		boolean result = registration.LastName("Sh");
+		Assert.assertEquals(false, result);
+	}
+	
+	@Test
+	public void givenLastName_WhenWithSpChars_ShouldReturnFalse() {
+		UserRegistration registration = new UserRegistration();
+		boolean result = registration.LastName("Shi$nde");
+		Assert.assertEquals(false, result);
+	}
+	
+	@Test
+	public void givenLastName_WhenWithoutCaps_ShouldReturnFalse() {
+		UserRegistration registration = new UserRegistration();
+		boolean result = registration.LastName("shinde");
+		Assert.assertEquals(false, result);
+	}
+	
+	//email id
+	@Test
+	public void givenEmailID_whenProper_shouldReturnTrue() {
+		UserRegistration registration = new UserRegistration();
+		boolean result = registration.EmailId("sagarika.shinde@yahoo.com");
+		Assert.assertEquals(true,result);
+	}
+	
+	@Test
+	public void givenEmailID_whenShortbutProper_shouldReturnTrue() {
+		UserRegistration registration = new UserRegistration();
+		boolean result = registration.EmailId("sagarika@yahoo.com");
+		Assert.assertEquals(true,result);
+	}
+	
+	@Test
+	public void givenEmailID_WhenEmailWithNumber_ShouldReturnTrue() {
+		UserRegistration registration = new UserRegistration();
+		boolean result = registration.EmailId("sagarika99@gmail.com");
+		Assert.assertEquals(true, result);
+	}
+	
+	@Test
+	public void givenEmailID_WhenTldStartsWithDot_ShouldReturnFalse() {
+		UserRegistration registration = new UserRegistration();
+		boolean result = registration.EmailId("sagarika@.com");
+		Assert.assertEquals(false, result);
+	}
+	
+	@Test
+	public void givenEmailID_WhenEmailStartsWithDot_ShouldReturnFalse() {
+		UserRegistration registration = new UserRegistration();
+		boolean result = registration.EmailId(".sagarika@.com");
+		Assert.assertEquals(false, result);
+	}
+	
+	@Test
+	public void givenEmailID_WhenEmailHasdoubleSymbol_ShouldReturnFalse() {
+		UserRegistration registration = new UserRegistration();
+		boolean result = registration.EmailId("sagarika@shinde@.com");
+		Assert.assertEquals(false, result);
+	}
+	
+	@Test
+	public void givenEmailID_WhenEmailTldHasdigit_ShouldReturnFalse() {
+		UserRegistration registration = new UserRegistration();
+		boolean result = registration.EmailId("sagarika@gmail.1om");
+		Assert.assertEquals(false, result);
+	}
+	
+	@Test
+	public void givenEmailID_WhenEmailCannotHaveMultipleTlds_ShouldReturnFalse() {
+		UserRegistration registration = new UserRegistration();
+		boolean result = registration.EmailId("sagarika@gmail.com.com.in");
+		Assert.assertEquals(false, result);
 	}
 }
